66303be77f97a090b3a0b6aedaa1f6466ed15ae4
Zhang Edward <Edward@ZhangmatoMac-Pro.local>
2012-01-05 12:40:38 +0800

    Initial Commit

---
+++ XMLParser/XMLParserAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  XMLParserAppDelegate.h
+//  XMLParser
+//
+//  Created by Edward Zhang on 12-1-5.
+//  Copyright (c) 2012年 uestc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class XMLParserViewController;
+
+@interface XMLParserAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) XMLParserViewController *viewController;
+
+@end
---
+++ XMLParser/XMLParserViewController.h
@@ -0,0 +1,13 @@
+//
+//  XMLParserViewController.h
+//  XMLParser
+//
+//  Created by Edward Zhang on 12-1-5.
+//  Copyright (c) 2012年 uestc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XMLParserViewController : UIViewController
+
+@end
